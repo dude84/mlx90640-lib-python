dbd8b4be9c30f1d210be1f9a424d1207a6186b8a
Maciej <maciej.rohleder@gmail.com>
2025-10-24 01:23:56 +0800

    remove wait

--- examples/src/test.cpp
+++ examples/src/test.cpp
@@ -134,7 +134,7 @@ int main(){
             }
             std::cout << std::endl;
         }
-        std::this_thread::sleep_for(std::chrono::milliseconds(20));
+        // std::this_thread::sleep_for(std::chrono::milliseconds(20));
         printf("\x1b[25A");
     }
     return 0;
